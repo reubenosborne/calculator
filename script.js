a9ee35d581b533206908f9d0b0791b0f3655a86c
Reuben Osborne <reuben.osborne@gmail.com>
2019-05-01 17:07:36 +1200

    Add pseudocode

---
+++ script.js
@@ -0,0 +1,50 @@
+
+
+
+// On Button Press //
+
+// If it's a number, add it to a variable
+
+// If it's a function, ignore?
+
+// If the next button pressed is a number, concatenate
+
+// If the next button pressed is a function, execute
+
+// If the next button pressed is a number, add to function forming
+
+
+/* Steps I can take */
+
+// 1. Have all buttons returning their value in console.log
+
+// 2. Apply logic to returned values
+
+
+// Do I need a container for the first number, the operator, and the second number?
+
+
+/* Event Listeners */
+
+    // Listen for button press and then:
+
+    // If it's a number, wait for a second input
+
+        // If the second input is another number then concatenate it to the first.
+
+        // Else if the second input is a function then store the first number as a variable and then store the function as a variable too then
+
+        // If the third input is another number then wait for any second number input then store it in a variable
+
+        // If equals is pressed then return equation.
+
+
+
+/* Views */
+
+// Display Number
+    // If a number is pressed, display it on the HTML page in the calculator display
+
+// Display Result
+    // If there's a result to display, then display it on the HTML page in the calculator display
+
